3a2caa34ad8eb174d6a9266a6c35632aec5ead56
SLAVA <SLAVA@SLAVAPC>
2017-07-10 19:53:40 +0600

    Исправлены замечания: В условиях разделяй сравнительные части пробелами Названия переменных тоже по camelCase

--- src/varianta/three/action/Action.java
+++ src/varianta/three/action/Action.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package varianta.three.action;
 
 import java.util.Scanner;
@@ -12,23 +8,23 @@ import java.util.Scanner;
  */
 public class Action {
     
-    String pattern = "(\\s)+|($)";
+    private String pattern = "(\\s)+|($)";
 
     /**
      *
      * @return
      */
-    public String[] Numbers() {
+    public String[] numbers() {
         boolean result;
         float number;
-        String[] masNum = null;
+        String[] arrayOfNumbers = null;
         try (Scanner scan = new Scanner(System.in)) {
             result = false;
             String str = scan.nextLine();
-            for (String retval : str.split(pattern)) {
+            for (String currentValue : str.split(pattern)) {
                 try {
-                    if (!retval.isEmpty()) {
-                        number = Float.valueOf(retval);
+                    if (!currentValue.isEmpty()) {
+                        number = Float.valueOf(currentValue);
                         result = true;
                     }
                 } catch (NumberFormatException e) {
@@ -39,34 +35,34 @@ public class Action {
             }
             scan.close();
             if (result) {
-                masNum = str.split(pattern);
+                arrayOfNumbers = str.split(pattern);
             }
         }
-        return masNum;
+        return arrayOfNumbers;
     }
     
     /**
      *
-     * @param mas
+     * @param arrayOfNumbers
      */
-    public void CompareNumbers(String[] mas) {
-        String strOutDec="";
-        String strOutInc="";
-        int AverageLength = 0;
-        for (String str:mas) {
-            AverageLength+=str.length();  
+    public void compareNumbers(String[] arrayOfNumbers) {
+        String strOutDec = "";
+        String strOutInc = "";
+        int averageLength = 0;
+        for (String str : arrayOfNumbers) {
+            averageLength += str.length();  
         }
-        AverageLength=AverageLength/mas.length;
+        averageLength = averageLength/arrayOfNumbers.length;
         
-        for (String str:mas) {
-            if (AverageLength>str.length()){
-               strOutDec=strOutDec+" "+str+"("+String.valueOf(str.length())+")";
+        for (String str : arrayOfNumbers) {
+            if (averageLength > str.length()){
+               strOutDec = strOutDec + " " + str + "(" + String.valueOf(str.length()) + ")";
              }
-            if (AverageLength<str.length()){
-               strOutInc=strOutInc+" "+str+"("+String.valueOf(str.length())+")";
+            if (averageLength < str.length()){
+               strOutInc = strOutInc + " " + str + "(" + String.valueOf(str.length()) + ")";
              }
         }
-        System.out.println("Числа, длина которых меньше средней длины:"+strOutDec);
-        System.out.println("Числа, длина которых больше средней длины:"+strOutInc);
+        System.out.println("Числа, длина которых меньше средней длины:" + strOutDec);
+        System.out.println("Числа, длина которых больше средней длины:" + strOutInc);
     }
 }
--- src/varianta/three/main/Runner.java
+++ src/varianta/three/main/Runner.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package varianta.three.main;
 
 import varianta.three.action.Action;
@@ -11,19 +7,16 @@ import varianta.three.action.Action;
  * @author Sukhocheva Maryana
  */
 public class Runner {
-        /**
-     *
-     */
-    
+       
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Введите несколько чисел и нажмите <Enter>:");
         Action action = new Action();
-        String[] masNum=action.Numbers();
-        if (masNum!=null) {
-        action.CompareNumbers(masNum);
+        String[] arrayOfNumbers = action.numbers();
+        if (arrayOfNumbers != null) {
+        action.compareNumbers(arrayOfNumbers);
         }
         
     }
